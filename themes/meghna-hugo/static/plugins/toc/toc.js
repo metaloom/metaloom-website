bd4f87146fe62bcd5d6f652e5fa5cbf4bbc6e56b
Johannes Schüth <git@jotschi.de>
2026-04-22 23:40:11 +0200

    Update dist

--- themes/meghna-hugo/static/plugins/toc/toc.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    var navSelector = "#toc";
-    var $toc = $(navSelector);
-    Toc.init($toc);
-    $("body").scrollspy({
-        target: navSelector
-    });
-});
--- watch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./build.sh ; hugo server -b http://blog:1313/
